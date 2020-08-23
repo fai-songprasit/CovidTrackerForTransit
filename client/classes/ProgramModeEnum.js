fd5abf37ffc14f1ac57c846e500671688c6a44ac
Gene Culling <culling66@gmail.com>
2020-08-24 01:11:53 +1200

    Socket messages now easier to test

---
+++ client/classes/ProgramModeEnum.js
@@ -0,0 +1,7 @@
+// Enums for the mode, currently just PROD and TEST
+const ProgramModeEnums= {
+    PROD: "prod",
+    TEST: "test",
+}
+
+export default ProgramModeEnums;
--- client/components/CovidPositiveBroadcast.jsx
+++ client/components/CovidPositiveBroadcast.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import Data from "./../classes/Data";
 import Trip from "../classes/Trip";
+import ProgramModeEnum from "./../classes/ProgramModeEnum";
+
+
 
 import socketIoClient from 'socket.io-client';
 const ENDPOINT = "localhost:3001";
+const MODE = ProgramModeEnum.PROD;
 const socket = socketIoClient(ENDPOINT);
 
 const CovidPositiveBroadcast = (props) => {
@@ -11,21 +15,33 @@ const CovidPositiveBroadcast = (props) => {
     let testInfectedData = {
         covidPositive: false,
     };
-    testInfectedData.trips = [{route:1, startTime: 1098181030220, endTime: 2098181030220, vehicleRef: 3706}]
+    testInfectedData.trips = [{ route: 1, startTime: 1098181030220, endTime: 2098181030220, vehicleRef: 3706 }]
 
     const [data, setData] = useState(Data.load());
 
     const positiveCovidTestClicked = () => {
-        // data.setCovidPositive(true);
-        const trips = testInfectedData.trips;
+        let trips;
+        if (MODE == ProgramModeEnum.TEST) {
+            // For testing
+            // data.setCovidPositive(true);
+            trips = testInfectedData.trips;
+        } else {
+            //PROD
+            data.setCovidPositive(true);
+            trips = data.trips;
+        }
+
         trips.forEach(trip => {
             console.log(trip);
             socket.emit('new state', trip);
         });
     }
     return (
-        <div className="container">
-            <button id="positiveCovidTest" onClick={positiveCovidTestClicked}>Positive COVID Test</button>
+        <div className="jumbotron">
+            <label>By clicking on this button you will alert others that a person who has tested positive for COVID shared a trip with them</label>
+            <div className="mx-auto mt-5">
+                <button id="positiveCovidTest" className="btn-danger btn btn-lg col sm12 align-middle" onClick={positiveCovidTestClicked}>Positive COVID Test</button>
+            </div>
         </div >
     )
 }
